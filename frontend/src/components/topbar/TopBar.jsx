8d37c3cb43a4785c79e915d7e88e090a44034cac
hitesh18-4 <hiteshbatra184@gmail.com>
2024-11-27 14:04:28 +0530

    Removed sidebar

--- frontend/src/components/topbar/TopBar.jsx
+++ frontend/src/components/topbar/TopBar.jsx
@@ -64,7 +64,7 @@ export default function TopBar() {
             </li>
           </ul>
         )}
-        <i className="topSearchIcon fas fa-search"></i>
+        
       </div>
     </div>
   );
